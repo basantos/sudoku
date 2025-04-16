8b2caa5291d60a9007adac43afaf479dbb302dc9
Brigitte Anne Santos <santosbrigitte@gmail.com>
2025-04-16 07:08:13 -0700

    Add check solution testcases

--- Sudoku_Testcases.py
+++ Sudoku_Testcases.py
@@ -4,8 +4,16 @@ import Sudoku
 
 
 class Test(unittest.TestCase):
-    def test_check_solution(self):
+    def test_check_solution_pass(self):
         b = Sudoku.Sudoku()
         solution = copy.deepcopy(b.get_solution())
         b.set_board()
         self.assertEqual(b.check_solution(), solution)
+        b.make_move('a1', 5)
+
+    def test_check_solution_not_pass(self):
+        b = Sudoku.Sudoku()
+        solution = copy.deepcopy(b.get_solution())
+        b.set_board()
+        b.make_move('a1', 10)
+        self.assertNotEqual(b.check_solution(), solution)
